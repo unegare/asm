a5e20cb7e2b361733fc54457b570bb12fc3d5902
unegare <dronte2008@yandex.ru>
2018-02-11 13:16:39 +0300

    ex_001: num of bytes

--- ex_001_server_client/server.asm
+++ ex_001_server_client/server.asm
@@ -44,6 +44,22 @@
 	syscall
 %endmacro
 
+%macro WRITE_NUM2MEM 3
+	xor rcx, rcx
+	mov rax, %2
+	mov r10, 10
+	mov r11, %1
+%%lp:	xor rdx, rdx
+	div r10
+	add dl, 48
+	mov byte [r11], dl
+	dec r11
+	inc rcx
+	test rax, rax
+	jnz %%lp
+	mov %3, rcx
+%endmacro
+
 ;-------------------------------------------------
 
 global _start
@@ -58,9 +74,13 @@ wfds resq fd_set_qwsize ;struct fd_set
 tv_sec resd 1
 tv_usec resq 1
 ;
-buffrecvlen resb 1
+buffrecvlen resq 1
 buff resb 258
 buffsize equ 256
+;
+mem4num resb 257
+mem4numlen resq 1
+mem4numsize equ 256
 
 section .data
 fd_set_bitsize equ 8*8*1024
@@ -103,8 +123,10 @@ isitlistener db "is it listener ... ", 0
 isitlistenerlen equ $-isitlistener
 accepting db "accepting ... ", 0
 acceptinglen equ $-accepting
-titlerecvmsg db "received message:", 10, 0
+titlerecvmsg db "received message: ", 0
 titlerecvmsglen equ $-titlerecvmsg
+wordbytes db " byte(s) ", 0
+wordbyteslen equ $-wordbytes
 lineend db 10, 0
 lineendlen equ $-lineend
 
@@ -163,15 +185,23 @@ _start:
 	FD_SET rfds, qword [listenerfd]
 
 	mov rax, [listenerfd]
+	inc rax
 	mov [sock_max], rax
 	mov dword [tv_sec], 15
 	mov qword [tv_usec], 0
+
+	mov byte [buff + buffsize], 0
+	mov qword [buffrecvlen], 0
+	mov byte [mem4num + mem4numsize], 0
+
+selectloop:
+;	mov dword [tv_sec], 15
+;	mov qword [tv_usec], 0
 
 	WRITE_STDOUT waitingforselect, waitingforselectlen
 
 	mov rax, __NR_select
 	mov rdi, [sock_max]
-	inc rdi
 	mov rsi, rfds
 	mov rdx, wfds
 	mov r10, 0
@@ -205,13 +235,21 @@ _start:
 	
 	WRITE_STDOUT okword, okwordlen
 	
-	mov byte [buff + buffsize], 0
-	
 	xor rax, rax
 	mov rdi, rbx
 	mov rsi, buffrecvlen
 	mov rdx, 1
 	syscall
+	
+	WRITE_STDOUT lineend, lineendlen
+	WRITE_STDOUT titlerecvmsg, titlerecvmsglen
+
+	WRITE_NUM2MEM (mem4num + mem4numsize -1), qword [buffrecvlen], qword [mem4numlen]
+	mov r9, mem4num + mem4numsize
+	sub r9, [mem4numlen]
+	WRITE_STDOUT r9, [mem4numlen]
+	
+	WRITE_STDOUT wordbytes, wordbyteslen
 
 	xor rax, rax
 	mov rdi, rbx
@@ -219,24 +257,32 @@ _start:
 	mov rdx, [buffrecvlen]
 	syscall
 
-	mov rdx, rax
+	mov r12, rax
+
+	WRITE_NUM2MEM (mem4num + mem4numsize -1), rax, qword [mem4numlen]
+	mov r9, mem4num + mem4numsize
+	sub r9, [mem4numlen]
+	WRITE_STDOUT r9, [mem4numlen]
+
+	WRITE_STDOUT wordbytes, wordbyteslen
+	WRITE_STDOUT lineend, lineendlen
+
 
 	mov rax, 3
 	mov rdi, rbx
 	syscall
 
-	lea rcx, [buff + rdx]
+	lea rcx, [buff + r12]
 	mov byte [rcx], 10
 	inc rcx
 	mov byte [rcx], 0
-	add rdx, 2
-	mov rcx, rax
+	add r12, 2
+
+	WRITE_STDOUT buff, r12
 
 	WRITE_STDOUT lineend, lineendlen
-	
-	WRITE_STDOUT titlerecvmsg, titlerecvmsglen
 
-	WRITE_STDOUT buff, rcx
+	jmp selectloop 
 
 	mov rax, 3
 	mov rdi, [listenerfd]
